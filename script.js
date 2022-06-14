33164846e0be9cbd022d6d33f30bc9cf49308236
unknown <nunezfrancisco400@gmail.com>
2022-06-13 20:56:08 -0400

    just finished the studio

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 // TODO: add code here
+//for (astronaut of json)
+
+window.addEventListener("load", function(){
+    let json = [];
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then( function(json){
+            const container = document.getElementById("container");
+            let counter = 1;
+            for (astronaut of json){
+                
+                container.innerHTML += `
+                <div class= "astronaut">
+                    <div class="bio">
+                    <h3>${counter}</h3>
+                        <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                        <p>Hours in space: ${astronaut.hoursInSpace}</p>
+                        <p class="status" >Active: ${astronaut.active}</p>
+                        <p>Skills: ${astronaut.skills} </p>
+                    </div>
+                    <img class="avatar" src=${astronaut.picture} alt="astronauts"></img>
+                </div>
+                
+                ` ;
+                if(astronaut.active === true){
+                    document.getElementsByClassName("status").style.color = "green";
+                }
+                counter++
+            }
+        });
+
+    });
+});
